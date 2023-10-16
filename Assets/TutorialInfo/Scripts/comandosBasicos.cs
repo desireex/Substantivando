ebe964b5f3c5d9f77142aa8fb88b798d8d732bca
caiosants <caiotobe@gmail.com>
2023-10-16 16:14:22 -0300

    Mudou a tela

--- Assets/TutorialInfo/Scripts/comandosBasicos.cs
+++ Assets/TutorialInfo/Scripts/comandosBasicos.cs
@@ -1,10 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
-public class comandosBasicos : MonoBehaviour
-{
-    public void carregaCena(string nomeCena){
-        Application.LoadLevel(nomeCena);
+
+public class MainMenuUI : MonoBehaviour{
+    public void Play()
+    {
+        Debug.Log("Play");
+    }
+    public void DontPLay(){
+        Debug.Log("A imagem não atrabalha");
+    }
+    public void MudaTela(string nomecena){
+        SceneManager.LoadScene(nomecena);
     }
 }
+//
