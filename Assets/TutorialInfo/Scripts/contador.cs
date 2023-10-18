24bdae76acccd90e7ebe0cf07615ec507dc8fe9d
caiosants <caiotobe@gmail.com>
2023-10-18 16:40:05 -0300

    mudança da tela principal

---
+++ Assets/TutorialInfo/Scripts/contador.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class contador : MonoBehaviour
+{
+  private float tempo;
+  public string nomeDaCena;  
+    // Start é chamado antes do primeiro quadro atualizar
+    public void Mudatela(string cena)
+    {
+        SceneManager.LoadScene(cena);
+    }
+
+    // Update é chamado a cada quadro
+    void Update()
+    {
+        tempo += Time.deltaTime;
+        if (tempo >= 3)
+        {
+            Mudatela(nomeDaCena);
+        }
+    }
+}
