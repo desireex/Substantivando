72b15b2837425ea15040277fd5dc72339796d33c
caiosants <caiotobe@gmail.com>
2023-10-26 13:05:43 -0300

    tela de temas

---
+++ Assets/TutorialInfo/Scripts/temaJogo.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class temaJogo : MonoBehaviour
+{
+    public Button btnFase;
+    public Button btnPlay;
+    public Text txtNomeDoTema;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
