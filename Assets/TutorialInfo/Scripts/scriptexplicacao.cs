531cc422a9d756c06ff91abb39707c2d63d078b4
desireex <desireex51@gmail.com>
2023-10-28 21:11:56 -0300

    tela de explicação1 resolvida!

--- Assets/TutorialInfo/Scripts/scriptexplicacao.cs
+++ Assets/TutorialInfo/Scripts/scriptexplicacao.cs
@@ -1,18 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using static UnityEditor.PlayerSettings;
 
 public class scriptexplicacao : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Button btnProximo;
+    public Button btnVoltar;
+    private int contaClicks;
+    public GameObject Explicacao;
+    public GameObject Piscadela;
+    public GameObject Vamos;
+    public string cenaPergunta;
+    public string cenaTemas;
     void Start()
     {
-        
+        contaClicks = 0;
+        Explicacao.SetActive(true);
+        Vamos.SetActive(false);
+        Piscadela.SetActive(false);
+        btnProximo.onClick = new Button.ButtonClickedEvent();
+        btnProximo.onClick.AddListener(ControlaBotaoProx);
+        btnVoltar.onClick = new Button.ButtonClickedEvent();
+        btnVoltar.onClick.AddListener(ControlaBotaoVoltar);
     }
 
-    // Update is called once per frame
-    void Update()
+    void ControlaBotaoProx()
     {
-        
+        if (contaClicks == 0) {
+            Explicacao.SetActive(false);
+            Vamos.SetActive(true);
+            Piscadela.SetActive(true);
+            contaClicks++;
+        }
+        else if(contaClicks == 1)
+        {
+            SceneManager.LoadScene(cenaPergunta);
+        }
+    }
+
+    void ControlaBotaoVoltar()
+    {
+        if(contaClicks == 0)
+        {
+            SceneManager.LoadScene(cenaTemas);
+        }
+        else if (contaClicks == 1)
+        {
+            Explicacao.SetActive(true);
+            Vamos.SetActive(false);
+            Piscadela.SetActive(false);
+            contaClicks--;
+        }
     }
 }
