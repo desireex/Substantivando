299aba381df1818f1d4da43477e304b0ae15f4de
caiosants <caiotobe@gmail.com>
2023-11-01 16:25:08 -0300

    script responder

--- Assets/TutorialInfo/Scripts/responder.cs
+++ Assets/TutorialInfo/Scripts/responder.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 using TMPro;
 public class responder : MonoBehaviour
 {
+    public int idTema;
     public TMP_Text pergunta;
     public TMP_Text respostaA;
     public TMP_Text respostaB;
@@ -16,11 +17,19 @@ public class responder : MonoBehaviour
     public string[] alternativaB;
     public string[] alternativaC;
     public string[] alternativaD;
-
-    // Start is called before the first frame update
+    public string[] corretas; //armazenamento de respostas certas
+    private int idPergunta;
+    private int acertos;
+    private int questoes;
     void Start()
     {
-        
+        idPergunta = 0;
+        questoes = perguntas.Length;
+        pergunta.text = perguntas[idPergunta];
+        respostaA.text = alternativaA[idPergunta];
+        respostaB.text = alternativaB[idPergunta];
+        respostaC.text = alternativaC[idPergunta];
+        respostaD.text = alternativaD[idPergunta];   
     }
 
     // Update is called once per frame
@@ -28,4 +37,33 @@ public class responder : MonoBehaviour
     {
         
     }
+public void resposta(string alternativa){
+    Debug.Log(alternativa);
+    switch (alternativa)
+    {
+        case "A":
+            if(alternativaA[idPergunta] == corretas[idPergunta]){
+                acertos += 1;
+            }
+        break;
+        case "B":
+            if(alternativaB[idPergunta] == corretas[idPergunta]){
+                acertos += 1;
+                Debug.Log("acerto");
+            }
+        break;
+        case "C":
+            if(alternativaC[idPergunta] == corretas[idPergunta]){
+                acertos += 1;
+            }
+        break;
+        case "D":
+            if(alternativaD[idPergunta] == corretas[idPergunta]){
+                acertos += 1;
+            }
+        break;
+    }
+}
+
+
 }
