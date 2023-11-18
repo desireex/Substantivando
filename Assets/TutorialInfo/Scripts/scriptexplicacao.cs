486fc238fcb414092c654af0cb8f937cb1773730
desireex <desireex51@gmail.com>
2023-11-18 14:52:16 -0300

    mais coisas

--- Assets/TutorialInfo/Scripts/scriptexplicacao.cs
+++ Assets/TutorialInfo/Scripts/scriptexplicacao.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-using static UnityEditor.PlayerSettings;
 
 public class scriptexplicacao : MonoBehaviour
 {
